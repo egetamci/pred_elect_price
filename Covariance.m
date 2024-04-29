67c425bbeec3db7230419c8aa413d88f1fc01034
Ege Tamcı <160476027+egetamci@users.noreply.github.com>
2024-04-29 08:21:58 +0300

    Update Covariance.m

--- Covariance.m
+++ Covariance.m
@@ -1,19 +1,25 @@
-function L_M1 = Covariance(X1, p)
-% Computes the covariance sequence of a given signal.
-% Inputs:
-%   - X1: Input signal vector
-%   - p: Number of lags for computing covariance
-% Output:
-%   - L_M1: Covariance sequence
-x_l = length(X1);
-Top_X = sum(abs(X1));
-for i2 = 1:p
-L_T1 = 0;
-for k3 = i2 + 1:x_l
-L_T1_aa = X1(k3) * sign(X1(k3 - i2));
-L_T1 = L_T1 + L_T1_aa;
+function covariance_sequence = Covariance(X, num_lags)
+% Covariance - Computes the covariance sequence of a given signal.
+%   This function computes the covariance sequence of a given signal vector
+%   up to a specified number of lags.
+%
+%   Inputs:
+%     - X: Input signal vector
+%     - num_lags: Number of lags for computing covariance
+%
+%   Output:
+%     - covariance_sequence: Covariance sequence
+
+    signal_length = length(X);
+    total_signal_abs_sum = sum(abs(X));
+    for lag = 1:num_lags
+        lag_sum = 0;
+        for k = lag + 1:signal_length
+            lag_product = X(k) * sign(X(k - lag));
+            lag_sum = lag_sum + lag_product;
+        end
+        covariance_sequence(lag) = lag_sum;
+    end
+    covariance_sequence = covariance_sequence ./ total_signal_abs_sum;
 end
-L_M1(i2) = L_T1;
-end
-L_M1 = L_M1 ./ Top_X;
-end
+
