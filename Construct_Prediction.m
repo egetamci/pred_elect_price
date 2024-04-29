7c67cfa69804482f0ce0c88ecda0ee981fde3246
Ege Tamcı <160476027+egetamci@users.noreply.github.com>
2024-04-29 08:20:43 +0300

    Update Construct_Prediction.m

--- Construct_Prediction.m
+++ Construct_Prediction.m
@@ -1,41 +1,48 @@
-function YY1 = Construct_Prediction(num_periods_1, aa_matrix_1, artificial_noise_1, hours_1, forecast_horizon_1, adjusted_data_copy_1)
-  % Construct_Prediction - Construct Prediction using Coefficients and Noise
-%   Construct_Prediction function constructs the prediction for the future data
-%   using coefficients and noise.
+function future_prediction = Construct_Prediction(periods_count, coefficients_matrix, artificial_noise, hours_per_period, forecast_horizon, adjusted_data_series)
+% Construct_Prediction - Constructs a prediction for future data using coefficients and noise
+%   This function constructs a prediction for future data based on past data,
+%   using a combination of coefficients and artificial noise.
 %
 %   Inputs:
-%     - num_periods_1: Number of periods
-%     - aa_matrix_1: Coefficients matrix
-%     - artificial_noise_1: Artificial noise data
-%     - hours_1: Hours
-%     - forecast_horizon_1: Forecast horizon
-%     - adjusted_data_copy_1: Adjusted data series
+%     - periods_count: Number of past periods used for prediction
+%     - coefficients_matrix: Matrix containing coefficients obtained from past data
+%     - artificial_noise: Artificial noise data added to improve prediction accuracy
+%     - hours_per_period: Number of hours in each period of the data
+%     - forecast_horizon: Number of future periods to forecast
+%     - adjusted_data_series: Adjusted historical data series used in prediction
 %
 %   Output:
-%     - YY1: Constructed prediction for the future data
-%
-  k_pp = 1;
-    if -num_periods_1 + forecast_horizon_1 * k_pp >= num_periods_1
-        k_pp = k_pp;
+%     - future_prediction: Prediction for future data
+
+    k = 1;
+    % Adjust k value based on forecast horizon and periods_count
+    if -periods_count + forecast_horizon * k >= periods_count
+        k = k;
     else
-        k_pp = k_pp + 1;
+        k = k + 1;
     end
-    
-    peri = num_periods_1 + 1;
-    for i5 = 1:forecast_horizon_1 * hours_1 - num_periods_1
-        if peri == forecast_horizon_1 + 1
-            peri = 1;
+
+    period_length = periods_count + 1;
+    % Construct the matrix Q for prediction
+    for i = 1:forecast_horizon * hours_per_period - periods_count
+        if period_length == forecast_horizon + 1
+            period_length = 1;
         end
-        for j3 = 1:num_periods_1 + 1
-            Q_Mat(i5, i5 + j3 - 1) = -aa_matrix_1(num_periods_1 + 1 - j3 + 1, peri);
+        % Fill the Q matrix with appropriate coefficients
+        for j = 1:periods_count + 1
+            Q_Matrix(i, i + j - 1) = -coefficients_matrix(periods_count + 1 - j + 1, period_length);
         end
-        peri = peri + 1;
+        period_length = period_length + 1;
     end
-    Q_Mat1 = Q_Mat(:, num_periods_1 + 1:end);
-    YY(num_periods_1 + 1:(forecast_horizon_1 * hours_1), 1) = inv(Q_Mat1)* artificial_noise_1(num_periods_1 + 1:(forecast_horizon_1 * hours_1),1);
-    for i22 = 1:num_periods_1
-        aa_f2 = flip(aa_matrix_1(2:num_periods_1 + 1, i22));
-        YY(i22, 1) = artificial_noise_1(i22, 1) + dot(adjusted_data_copy_1(k_pp * forecast_horizon_1- num_periods_1 + i22:k_pp * forecast_horizon_1 - 1 + i22), aa_f2);
+    % Slice the Q matrix to get the appropriate section
+    Q_Matrix1 = Q_Matrix(:, periods_count + 1:end);
+    % Calculate prediction using matrix inversion
+    YY(periods_count + 1:(forecast_horizon * hours_per_period), 1) = inv(Q_Matrix1) * artificial_noise(periods_count + 1:(forecast_horizon * hours_per_period), 1);
+    % Calculate predictions for the initial periods
+    for i = 1:periods_count
+        coefficients = flip(coefficients_matrix(2:periods_count + 1, i));
+        YY(i, 1) = artificial_noise(i, 1) + dot(adjusted_data_series(k * forecast_horizon - periods_count + i:k * forecast_horizon - 1 + i), coefficients);
     end
-    YY1 = YY;
+    % Output the future prediction
+    future_prediction = YY;
 end
